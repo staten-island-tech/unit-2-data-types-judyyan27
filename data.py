237d606a94f0df73b527b476f79115d9501d848a
Judy <judyy10@nycstudents.net>
2024-02-05 13:57:15 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,8 +1,41 @@
-x = 3
-y = float(3)
-print(x,y)
+# x = 3
+# y = float(3)
+# print(x,y)
 
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values:
-    print(i)
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#    print(i)
+
+# animals = ["Zebra", "Ape", "Monkeys"]
+# print(animals[0])
+
+# """ for animal in animals:
+#    if(animal == "Ape"):
+#        print(animal)
+
+# name = "Judy"
+# print(name[0])
+#x = name.lower()
+# print(x) 
+
+# message = "Insert cool catchphrase here"
+# words = message.split( )
+# print(len(words))
+# y=input("give me a number")
+# *cant add string to a number [error]
+# print(y)
+# print(int(y) + 9)
+
+message = "Please input number below"
+words = message.split( )
+
+# input = 
+value = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31]
+x = "13"
+
+if x == value[0-15]:
+    print("odd")
+    
+else:
+    print("even")
